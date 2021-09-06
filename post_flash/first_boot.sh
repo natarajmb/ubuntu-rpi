7a794914a5bc321fe12488414638af37016e472c
Nataraj Basappa <n5j.b5a@gmail.com>
2021-09-06 14:03:34 +0100

    Inital commit of RPI vanilla ubuntu build

---
+++ post_flash/first_boot.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# update wireless settings
+if test -f wireless.yaml; then
+    echo "First boot - updating wireless settings"
+    cat /wireless.yaml >> /etc/netplan/50-cloud-init.yaml
+    netplan generate
+fi
+
+# update hostname
+echo "First boot - updating hostname" 
+echo "YOUR_HOSTNAME" > '/etc/hostname'
+
+# letting iptable see bridge traffic and allowing ip forwarding
+cat <<EOF | tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+net.ipv4.tcp_mtu_probing = 1
+EOF
+
+# enable cgroup memory
+echo "First boot - enable cgroup memory & cpu limit"
+sed -i '$ s/$/ cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1/' /boot/firmware/cmdline.txt
+
+# disable service by deleting self
+
+if test -f wireless.yaml; then
+    rm /wireless.yaml
+fi
+rm $0
+
+# reboot system
+reboot now
